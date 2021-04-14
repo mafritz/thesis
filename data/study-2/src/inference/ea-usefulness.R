027bb9e8872c5f43e54b5f44232260397f9cf8e6
Mattia A. Fritz <mattia.fritz@unige.ch>
2021-04-14 20:13:56 +0200

    Moved participants to corollary analysis

--- data/study-2/src/inference/ea-usefulness.R
+++ data/study-2/src/inference/ea-usefulness.R
@@ -57,22 +57,3 @@ s2.mlm.ea_usefulness.comparison <- emmeans(
   ) +
   scale_color_viridis_d() +
   NULL)
-
-s2.participant_plus_20_emotions <- s2.participants_aggregated %>% 
-  filter(num_emotions >= 20, !is.na(Expectancy), !is.na(Final))
-
-(s2.ea_usefulness.topexpress.graph <- s2.ea_usefulness %>% 
-  filter(participant %in% s2.participant_plus_20_emotions$participant) %>%
-  left_join(s2.participant_plus_20_emotions) %>% 
-  ggplot(aes(x = as_factor(abbreviate(survey)), y = value, color = dimension, group = dimension, label = num_emotions)) + 
-  geom_line(size = 2) +
-  geom_point(size = 4) +
-  geom_dl(aes(label = num_emotions), method = list(dl.combine("last.points")), cex = 20) +
-  facet_wrap(~participant) +
-  theme(legend.position = "none") +
-  scale_color_viridis_d() +
-  labs(
-    x = "Survey",
-    y = "Rating"
-  ) +
-  NULL)
