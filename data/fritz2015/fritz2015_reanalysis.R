3e01551384b2e280d3fff17ed5d0de8627878218
Mattia A. Fritz <mattia.fritz@unige.ch>
2022-06-26 15:28:03 +0200

    Results of UX test

--- data/fritz2015/fritz2015_reanalysis.R
+++ data/fritz2015/fritz2015_reanalysis.R
@@ -5,6 +5,7 @@ library(papaja)
 library(afex)
 library(performance)
 library(report)
+library(corrr)
 
 fritz2015_survey <- read_delim(here::here("data/fritz2015/fritz2015_survey.csv"), delim = ";", escape_double = FALSE, trim_ws = TRUE, col_types = cols(Code = col_character())) |> 
   mutate(
@@ -22,12 +23,21 @@ fritz2015_excluded <- fritz2015_eyetracking |>
 fritz2015_eyetracking_filtered <- fritz2015_eyetracking |> 
   filter(!user %in% fritz2015_excluded$user)
 
+fritz2015_eyetracking_filtered <- fritz2015_eyetracking_filtered |> 
+  mutate(across(-c(X1, user), ~as.numeric(gsub("-", "", .))))
+
 fritz2015_emotions_filtered <- fritz2015_emotions |> 
   filter(!user %in% fritz2015_excluded$user)
 
 fritz2015_survey_filtered <- fritz2015_survey |> 
   filter(!user %in% fritz2015_excluded$user)
 
+ux.appraisals <- fritz2015_emotions_filtered |> 
+  group_by(user) |> 
+  summarise(
+    n = n(),
+    cor = cor(x, y)
+  )
 
 ux.expressing <- fritz2015_emotions_filtered |> 
   group_by(user, click) |>
@@ -39,23 +49,41 @@ ux.expressing <- fritz2015_emotions_filtered |>
     tot = button + other + none
   )
 
-ux.click_type.test <- t.test(
-  ux.expressing$button,
-  ux.expressing$other,
-  alternative = "greater",
-  paired = "TRUE",
-)
-
-ux.displaying <- fritz2015_eyetracking_filtered |> 
-  select(user, Total_Visit_Duration_Displaying_Sum, Mouse_Click_Count_Displaying_Sum, et_sample) |> 
-  left_join(fritz2015_survey, by = "user") |> 
+ux.perceiving <- fritz2015_eyetracking_filtered |> 
   transmute(
     user = user,
-    et_sample = et_sample,
-    TotEmo = TotEmo,
-    duration = Total_Visit_Duration_Displaying_Sum,
-    time_per_emotion = duration / TotEmo,
-    numer_click = Mouse_Click_Count_Displaying_Sum / TotEmo
+    timeline_visits = Visit_Count_Timeline_Sum,
+    timeline_duration = Total_Visit_Duration_Timeline_Sum,
+    linechart_visits = Visit_Count_Graphique_1_Sum + Visit_Count_Graphique_2_Sum,
+    linechart_duration = Total_Visit_Duration_Graphique_1_Sum + Total_Visit_Duration_Graphique_2_Sum
   )
 
-ux.expressing_not_listed <- fritz2015_emotions_filtered |> filter(listed == 0, feeling != "None")
+ux.sus_scale_long <- fritz2015_survey_filtered |> 
+  select(user, SUS1:SUS10) |> 
+  pivot_longer(-user, names_to = "item", values_to = "evaluation") |> 
+  mutate(
+    item = factor(item, levels = c(
+      "SUS1",
+      "SUS2",
+      "SUS3",
+      "SUS4",
+      "SUS5",
+      "SUS6",
+      "SUS7",
+      "SUS8",
+      "SUS9",
+      "SUS10"
+    )),
+    order = as.numeric(str_remove(item, "SUS")),
+    score = if_else(
+      order %% 2 != 0,
+      evaluation - 1,
+      7 - evaluation
+    )
+  )
+
+ux.sus_total <- ux.sus_scale_long |> 
+  group_by(user) |> 
+  summarise(
+    score = sum(score) * 10/6
+  )
