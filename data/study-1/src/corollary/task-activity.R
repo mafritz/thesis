a796c4afcb7ea99126d9a2649ae293ea6c304cfc
Mattia A. Fritz <mattia.fritz@unige.ch>
2022-07-05 19:27:43 +0200

    Improved graphics and data for task part

--- data/study-1/src/corollary/task-activity.R
+++ data/study-1/src/corollary/task-activity.R
@@ -19,23 +19,25 @@ s1.emotion_per_task_activity <- s1.dew_emotions |>
   ungroup() |> 
   group_by(group, enigma_situation) |> 
   summarise(
-    M = mean(N),
-    SD = sd(N)
+    observed = mean(N)
   ) |>
   mutate(
-    Projection = case_when(
-      enigma_situation == "Reading" ~ (M / 40) * 300,
-      enigma_situation == "Solving" ~ (M / 200) * 300,
-      TRUE ~ (M / 60) * 300
+    projected = case_when(
+      enigma_situation == "Reading" ~ (observed / 40) * 300,
+      enigma_situation == "Solving" ~ (observed / 200) * 300,
+      TRUE ~ (observed / 60) * 300
     )
   ) |> 
-  arrange(enigma_situation)
+  arrange(enigma_situation) |> 
+  pivot_longer(
+    -c(group, enigma_situation)
+  )
 
-s1.emotion_per_task_activity.graph <- ggplot(s1.emotion_per_task_activity, aes(x = group, y = Projection, fill = group)) +
-  geom_bar(stat = "identity") +
-  facet_wrap(~enigma_situation) +
+s1.emotion_per_task_activity.graph <- ggplot(s1.emotion_per_task_activity, aes(x = group, y = value, fill = group)) +
+  geom_bar(stat = "identity", position = position_dodge2(width = 0.5, preserve = "single", padding = 0.5)) +
+  facet_grid(name~enigma_situation) +
   scale_fill_viridis_d() +
   theme(legend.position = "none") +
-  labs(x = NULL, y = "Projected number of expressed emotions") +
+  labs(x = NULL, y = "Number of expressed emotions") +
   geom_hline(yintercept = 13.8, color = "gray", size = 2, alpha = 0.7) +
   NULL
