86a7d1d771680b0b201a25eed1c81c01c6137024
Mattia A. Fritz <mattia.fritz@unige.ch>
2023-03-14 10:58:59 +0100

    Changed color palette for sim

--- data/dew-rt/src/02-eda.R
+++ data/dew-rt/src/02-eda.R
@@ -1,6 +1,7 @@
 # Exploratory data analysis DEW simulations
 
 library(here)
+library(see)
 source(here("data/dew-rt/src/01-wrangle.R"))
 
 theme_set(theme_apa(box = TRUE))
@@ -21,7 +22,7 @@ dew_rt.sim_gew_graph_3 <- dew_rt.sim_gew |>
   theme(
     legend.position = "right"
   ) +
-  scale_fill_brewer(palette = "Dark2") +
+  scale_fill_flat_d() +
   facet_wrap(~space, strip.position = "bottom") +
   coord_flip() +
   NULL
@@ -42,49 +43,49 @@ dew_rt.sim_gew_graph_10 <- dew_rt.sim_gew |>
   theme(
     legend.position = "right"
   ) +
-  scale_fill_brewer(palette = "Dark2") +
+  scale_fill_flat_d() +
   facet_wrap(~space, strip.position = "bottom") +
   coord_flip() +
   NULL
 
-dew_rt.sim_gillioz_et_al_graph_3 <- dew_rt.sim_gillioz_et_al |> 
-  filter(
-  as.numeric(order) <= 3
-) |>
-  ggplot(
-    aes(x = fct_rev(label), fill = order)
-  ) +
-  geom_bar(position = position_stack(reverse = TRUE)) +
-  labs(
-    y = "Number of appearances with 3 suggestions",
-    x = NULL,
-    fill = "Position"
-  ) +
-  theme(
-    legend.position = "right"
-  ) +
-  scale_fill_brewer(palette = "Dark2") +
-  facet_wrap(~space, strip.position = "bottom") +
-  coord_flip() +
-  NULL
-
-dew_rt.sim_gillioz_et_al_graph_10 <- dew_rt.sim_gillioz_et_al |> 
-  filter(
-  as.numeric(order) <= 10
-) |>
-  ggplot(
-    aes(x = fct_rev(label), fill = order)
-  ) +
-  geom_bar(position = position_stack(reverse = TRUE)) +
-  labs(
-    y = "Number of appearances with 10 suggestions",
-    x = NULL,
-    fill = "Position"
-  ) +
-  theme(
-    legend.position = "right"
-  ) +
-  scale_fill_brewer(palette = "Dark2") +
-  facet_wrap(~space, strip.position = "bottom") +
-  coord_flip() +
-  NULL
+# dew_rt.sim_gillioz_et_al_graph_3 <- dew_rt.sim_gillioz_et_al |> 
+#   filter(
+#   as.numeric(order) <= 3
+# ) |>
+#   ggplot(
+#     aes(x = fct_rev(label), fill = order)
+#   ) +
+#   geom_bar(position = position_stack(reverse = TRUE)) +
+#   labs(
+#     y = "Number of appearances with 3 suggestions",
+#     x = NULL,
+#     fill = "Position"
+#   ) +
+#   theme(
+#     legend.position = "right"
+#   ) +
+#   scale_fill_flat_d() +
+#   facet_wrap(~space, strip.position = "bottom") +
+#   coord_flip() +
+#   NULL
+# 
+# dew_rt.sim_gillioz_et_al_graph_10 <- dew_rt.sim_gillioz_et_al |> 
+#   filter(
+#   as.numeric(order) <= 10
+# ) |>
+#   ggplot(
+#     aes(x = fct_rev(label), fill = order)
+#   ) +
+#   geom_bar(position = position_stack(reverse = TRUE)) +
+#   labs(
+#     y = "Number of appearances with 10 suggestions",
+#     x = NULL,
+#     fill = "Position"
+#   ) +
+#   theme(
+#     legend.position = "right"
+#   ) +
+#   scale_fill_flat_d() +
+#   facet_wrap(~space, strip.position = "bottom") +
+#   coord_flip() +
+#   NULL
